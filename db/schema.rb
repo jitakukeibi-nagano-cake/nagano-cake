2c7ec88f26e0e0fa92aae42989ed6bccaa885fc8
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-05-18 10:49:31 +0000

    [update]oder_details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_18_080557) do
+ActiveRecord::Schema.define(version: 2023_05_18_095126) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,16 +64,14 @@ ActiveRecord::Schema.define(version: 2023_05_18_080557) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  create_table "oder_ditails", force: :cascade do |t|
+    t.integer "oder_id", null: false
+    t.integer "item_", null: false
+    t.integer "quantity", null: false
+    t.integer "making_status", null: false
+    t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
