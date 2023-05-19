333ac924657e6f2bf8bcc84b04abffe01d828505
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-19 02:13:51 +0000

    items

--- db/migrate/20230518093421_create_items.rb
+++ db/migrate/20230518093421_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.string :name, null: false
-      t.interger :price, null: false
+      t.integer :price, null: false
       t.text :introduction, null: false
       t.boolean :is_active, default: true, null: false
       t.integer :genre_id, null: false
