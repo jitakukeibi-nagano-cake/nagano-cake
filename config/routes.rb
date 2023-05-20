7b64ca4febcc945eb2e76363bf27474716e1ec0b
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-20 04:26:55 +0000

    routes内items関係追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+  end
+  
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
