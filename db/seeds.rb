7e8de4c323427afc60402ee24005fb1038849f04
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-20 06:53:36 +0000

    seed.rb追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ゴリ作成
+# 管理者のログイン時の初期値設定
+Admin.create!(
+   email: "admin@naganocake.com",
+   password: "admin1111"
+)
