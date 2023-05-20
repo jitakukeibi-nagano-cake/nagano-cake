2ffeebd1ca54d629b1718ddc7aaef4fd2ee84322
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-20 12:44:21 +0000

    adminトップページ大枠作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,10 +2,10 @@ class Admin::OrderController < ApplicationController
     
     
     def index
-        
+        @orders = Order.page(params[:page])
     end
     
     def show
-        
+        @order = Order.find(params[:id])
     end
 end
