6dcb17923e6781235b27f3c6c693708cd97243c3
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-21 14:16:58 +0000

    5/21/23:16

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
+ # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
 namespace :admin do
     root to: 'homes#top'
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
@@ -18,18 +31,5 @@ end
     root to: 'homes#top'
     get '/about' => 'homes#about'
   end
-
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
 
 end
