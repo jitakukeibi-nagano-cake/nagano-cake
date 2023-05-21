084306fbd1a8a62d302536d16c4928b814bed7b1
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-21 05:17:10 +0000

    admin/genre関係追記

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,28 +1,27 @@
 class Admin::GenresController < ApplicationController
     
-    
     def index
         @category = Category.new
         @categories = Category.all
     end
     
     def edit
-        
+
     end
-    
+
     def create
         @category = Category.new(category_params)
         @category.save
         flash[:notice] = "#{@category.name}を追加しました"
         redirect_to admin_genres_path
     end
-    
+
     def update
-        
+
     end
-    
+
     private
-    
+
     def category_params
         params.require(:category).permit(:name)
     end
