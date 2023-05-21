37c4687a3f308349c3f49a4ca6d0f0203c945a36
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-21 09:02:37 +0000

    コントローラとテンプレート作成

---
+++ app/controllers/public/delivery_address_controller.rb
@@ -0,0 +1,15 @@
+class Public::DeliveryAddressController < ApplicationController
+  
+  def index
+  
+  end 
+  
+  def edit
+  
+  end 
+  
+  def update
+  
+  end 
+  
+end
---
+++ app/helpers/public/delivery_address_helper.rb
@@ -0,0 +1,2 @@
+module Public::DeliveryAddressHelper
+end
