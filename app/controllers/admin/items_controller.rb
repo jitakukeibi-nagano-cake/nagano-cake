691cbb9133fa6c26a50b964dbf1e94fa70123e52
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-22 07:15:57 +0000

    itmes関係色々追記

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,26 +2,42 @@ class Admin::ItemsController < ApplicationController
     # ゴリ作成
     
     def index
-        
+    @items = Item.all
     end
-    
+      
     def new
-        
+        @item = Item.new
     end
-    
-    def create
-        
-    end
-    
+      
     def show
-        
+        @item = Item.find(params[:id])
     end
-    
+      
     def edit
-        
+        @item = Item.find(params[:id])
     end
-    
+      
+    def create
+        @item = Item.new(item_params)
+        if @item.save
+          redirect_to admin_item_path(@item)
+        else
+          render :new
+        end
+    end
+      
     def update
-        
+        @item = Item.find(params[:id])
+        if @item.update(item_params)
+          redirect_to admin_item_path(@item.id)
+        else
+          render :edit
+        end
     end
+      
+    private
+    def item_params
+        params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+    end
+      
 end
