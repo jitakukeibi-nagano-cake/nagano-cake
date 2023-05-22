8446cbc3c025ad8ce3b81e6fe0b15e04c368ff81
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-22 09:55:44 +0000

    itmes関係大枠完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,10 @@ class Item < ApplicationRecord
     has_many :cart_items, dependent: :destroy
     has_many :order_details, dependent: :destroy
     belongs_to :genre
+    
+    validates :name, presence: true
+    validates :introduction, presence: true, length: { minimum: 0, maximum: 500 }
+    validates :price, presence: true
 
     
 end
