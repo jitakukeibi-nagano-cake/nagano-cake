02f08eb004781ee24c36d8e04aab860d7bc70652
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-05-23 08:44:03 +0000

    注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,33 @@
-class Admin::OrderController < ApplicationController
+class Admin::OrdersController < ApplicationController
 
 
-    def index
-        @order = Order.all.page(params[:page])
+  def index
+    @order = Order.all.page(params[:page])
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:order_id])
+    @order_detail = OrderDetail.find(params[:id])
+    @order_details = @order.order_details.all
+
+    is_updated = true
+    if @order_detail.update(order_detail_params)
+      @order.update(status: 2) if @order_detail.making_status == "in_production"
+      # ②製作ステータスが「製作中」のときに、注文ステータスを「製作中」に更新する。
+      # ③紐付いている注文商品の製作ステータスが "すべて" [製作完了]になった際に注文ステータスを「発送準備中」に更新させたいので、
+      @order_details.each do |order_detail| #　紐付いている注文商品の製作ステータスを一つ一つeach文で確認していきます。
+        if order_detail.making_status != "production_complete" # 製作ステータスが「製作完了」ではない場合
+          is_updated = false # 上記で定義してあるis_updatedを「false」に変更する。
+        end
+      end
+      @order.update(status: 3) if is_updated
+      # is_updatedがtrueの場合に、注文ステータスが「発送準備中」に更新されます。上記のif文でis_updatedがfalseになっている場合、更新されません。
     end
+    redirect_to admin_order_path(@order)
+  end
 
-    def show
-        @order = Order.find(params[:id])
-    end
 end
