31ff4581ca9f47b5444eae262f33fc2ad7b4e778
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-05-23 07:10:55 +0000

    quit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  before_action :authenticate_customer!
+  before_action :authenticate_customer!, only: [:show, :quit, :out, :edit, :update]
   
   def show
     @customer = current_customer
@@ -13,7 +13,7 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
     if @customer.update(customer_params)
       flash[:notice] = "会員情報を更新しました。"
-      redirect_to customer_path
+      redirect_to customers_path
     else
       render :edit
     end
@@ -28,13 +28,13 @@ class Public::CustomersController < ApplicationController
     @customer.update(is_deleted: true)
     reset_session
     flash[:alert] = "退会処理が完了しました。"
-    redirect_to top_path
+    redirect_to root_path
   end
   
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone, :email)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
   end
   
 end
