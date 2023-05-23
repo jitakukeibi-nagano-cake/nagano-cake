fe7831d648477565a2878dfbe433f1be522ab3b8
IgenInamoto <szancd45d@i.softbank.jp>
2023-05-23 07:47:25 +0000

    adminトップページ追記

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,8 @@ class Admin::HomesController < ApplicationController
      before_action :authenticate_admin!
     
     def top
-         @orders = Order.all.page(params[:page])
+        @params = params[:id]
+        @order = Order.where(customer_id: @params).page(params[:page]).per(10).order(created_at: :desc)
+        @orders = Order.page(params[:page]).per(10).order(created_at: :desc)
     end
 end
