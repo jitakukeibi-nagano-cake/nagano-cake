4eaef7096796b55dadcf3243956fc8bd008d4282
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-25 08:21:12 +0000

    一旦保存

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,9 +3,6 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-  validates :item_id, presence: true
-  validates :customer_id, presence: true
-  validates :quantity, presence: true
 
   def subtotal
     item.with_tax_price * quantity
