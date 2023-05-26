754f924dc5079a0eb0396d496fd98aaa5a37c70c
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-26 07:03:42 +0000

    変更しました

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -19,7 +19,7 @@ class Public::OrdersController < ApplicationController
      if params[:order][:select_address] == "0"
       @order.postcode = current_customer.postcode
       @order.address = current_customer.address
-      @order.name = current_customer.last_name + current_customer.first_name
+      @order.name = current_customer.first_name + current_customer.last_name
      elsif params[:order][:select_address] == "1"
       delivery = DeliveryAddress.find(params[:order][:delivery_address_id])
       @order.postcode = delivery.postcode
