5b39c130bb31154e254667b55ee28a7545213bb2
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-26 04:45:41 +0000

    signup後マイページに遷移するように修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,7 +2,10 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+  def after_sign_up_path_for(resource)
+    customers_path
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -41,7 +44,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-   
+
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number])
   end
