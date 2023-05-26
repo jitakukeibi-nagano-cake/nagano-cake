ccba236813950cc2e9a99c84ba2f9b4025263abb
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-05-26 05:38:52 +0000

    ステータスno
    exit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,17 +8,17 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    # @customer = Customer.find(params[:id])
     @order_detail = OrderDetail.where(order_id: @order.id)
     @order_details = @order.order_details
   end
 
   def update
     @order = Order.find(params[:id])
-    @order_detail = OrderDetail.find(params[:id])
-    @order_details = @order.order_details.all
-    if @order.update(order_params)
-      @order_details.update_all(making_status: 1) if @order.status == "payment_confirmation"
+    @order_details = @order.order_details
+    @order.update(order_params)
+
+    if @order.status == "payment_confirmation"
+      @order_details.update_all(making_status: 1)
       ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
     end
     redirect_to admin_order_path(@order)
